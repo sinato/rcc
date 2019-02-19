a2a72350aab7b03f56b4ce79cee82c0b7ff0de9f
Shingo Kato <katomail0510@gmail.com>
2019-02-20 01:22:23 +0900

    Add lexer (but not use it)

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,37 @@ extern crate inkwell;
 use inkwell::context::Context;
 use std::{env, path, process};
 
-fn compile(code: String) {
+#[derive(Debug, PartialEq)]
+enum Token {
+    Num(u64),
+    Op(String),
+}
+
+fn lexer(code: String) -> Vec<Token> {
+    let elements = code.split(" ").collect::<Vec<&str>>();
+    println!("elements: {:?}", elements);
+
+    let mut tokens: Vec<Token> = Vec::new();
+    for element in elements.iter() {
+        let first_char = element
+            .chars()
+            .nth(0)
+            .expect("Lexing error: illigal input.");
+        if first_char.is_digit(10) {
+            let num = element
+                .parse::<u64>()
+                .expect(&format!("Expect a number, but got {}", element));
+            tokens.push(Token::Num(num));
+        } else if first_char == '+' {
+            tokens.push(Token::Op(element.to_string()));
+        } else {
+            panic!(format!("This token is not implemented: {:?}", element));
+        }
+    }
+    tokens
+}
+
+fn compiler(code: String) {
     // initialize
     let context = Context::create();
     let module = context.create_module("my_module");
@@ -31,5 +61,17 @@ fn main() {
     }
     let code = args[1].to_string();
     println!("code: {}", code);
-    compile(code);
+    compiler(code);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_lexer_add() {
+        let code = String::from("10 + 20");
+        let expect = vec![Token::Num(10), Token::Op(String::from("+")), Token::Num(20)];
+        let actual = lexer(code);
+        assert_eq!(actual, expect);
+    }
 }

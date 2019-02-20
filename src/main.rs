38cb8497cd1232a5a8edc39d8a4998747bf1d2f0
Shingo Kato <katomail0510@gmail.com>
2019-02-20 22:24:21 +0900

    Add parser (but not use it).

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate inkwell;
 use inkwell::context::Context;
 use std::{env, path, process};
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Token {
     Num(u64),
     Op(String),
@@ -31,6 +31,42 @@ fn lexer(code: String) -> Vec<Token> {
         }
     }
     tokens
+}
+
+#[derive(Debug, PartialEq)]
+struct AstExp {
+    op: Token,
+    lhs: Token,
+    rhs: Token,
+}
+#[derive(Debug, PartialEq)]
+struct AstNum {
+    num: Token,
+}
+
+#[derive(Debug, PartialEq)]
+enum Ast {
+    Exp(AstExp),
+    Num(AstNum),
+}
+
+fn parser(tokens: Vec<Token>) -> Ast {
+    match tokens.len() {
+        1 => match tokens[0] {
+            Token::Num(_) => Ast::Num(AstNum {
+                num: tokens[0].clone(),
+            }),
+            Token::Op(_) => panic!("Not implemented"),
+        },
+        3 => {
+            // TODO: token type validation
+            let lhs = tokens[0].clone();
+            let op = tokens[1].clone();
+            let rhs = tokens[2].clone();
+            Ast::Exp(AstExp { op, lhs, rhs })
+        }
+        _ => panic!("Not implemented"),
+    }
 }
 
 fn compiler(code: String) {
@@ -74,4 +110,34 @@ mod tests {
         let actual = lexer(code);
         assert_eq!(actual, expect);
     }
+
+    #[test]
+    fn test_parser_num() {
+        let expect = Ast::Num(AstNum {
+            num: Token::Num(2434),
+        });
+        let tokens = vec![Token::Num(2434)];
+        let actual = parser(tokens);
+        assert_eq!(actual, expect);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_parser_num_illegal() {
+        let tokens = vec![Token::Op(String::from("+"))];
+        parser(tokens);
+    }
+
+    #[test]
+    fn test_parser_exp() {
+        let expect = Ast::Exp(AstExp {
+            op: Token::Op(String::from("+")),
+            lhs: Token::Num(10),
+            rhs: Token::Num(20),
+        });
+        let tokens = vec![Token::Num(10), Token::Op(String::from("+")), Token::Num(20)];
+        let actual = parser(tokens);
+        assert_eq!(actual, expect);
+    }
+
 }

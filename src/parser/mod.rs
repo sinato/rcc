0e12654252362881d6715a5707a6810b80162222
Shingo Kato <katomail0510@gmail.com>
2019-02-22 09:55:50 +0900

    Re:Rewrite the parser.

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,25 +2,33 @@ use crate::lexer::Token;
 use inkwell::builder::Builder;
 use inkwell::context::Context;
 
-#[derive(Debug, PartialEq)]
-pub struct AstExp {
+#[derive(Debug, PartialEq, Clone)]
+pub struct AstBinaryExp {
     op: AstOp,
-    lhs: AstNum,
-    rhs: AstNum,
+    lhs: Box<AstNode>,
+    rhs: Box<AstNode>,
 }
-impl AstExp {
+impl AstBinaryExp {
     fn get_op_string(&self) -> String {
         self.op.get_op()
     }
     fn get_lhs_num(&self) -> u64 {
-        self.lhs.get_num()
+        let node = *self.lhs.clone();
+        match node {
+            AstNode::Num(ast_num) => ast_num.get_num(),
+            _ => panic!("Not implemented."),
+        }
     }
     fn get_rhs_num(&self) -> u64 {
-        self.rhs.get_num()
+        let node = *self.rhs.clone();
+        match node {
+            AstNode::Num(ast_num) => ast_num.get_num(),
+            _ => panic!("Not implemented."),
+        }
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct AstNum {
     num: Token,
 }
@@ -33,7 +41,7 @@ impl AstNum {
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct AstOp {
     op: Token,
 }
@@ -46,15 +54,15 @@ impl AstOp {
     }
 }
 
-#[derive(Debug, PartialEq)]
-pub enum Ast {
-    Exp(AstExp),
+#[derive(Debug, PartialEq, Clone)]
+pub enum AstNode {
+    Exp(AstBinaryExp),
     Num(AstNum),
 }
-impl Ast {
+impl AstNode {
     pub fn emit(&self, context: &Context, builder: &Builder) {
         match self {
-            Ast::Exp(ast_exp) => {
+            AstNode::Exp(ast_exp) => {
                 let op = ast_exp.get_op_string();
                 if op == String::from("+") {
                     let i32_type = context.i32_type();
@@ -66,7 +74,7 @@ impl Ast {
                     panic!("Emit Error: Not implemented operator");
                 }
             }
-            Ast::Num(ast_num) => {
+            AstNode::Num(ast_num) => {
                 let num = ast_num.get_num();
                 let a = context.i32_type().const_int(num, false);
                 builder.build_return(Some(&a));
@@ -75,43 +83,61 @@ impl Ast {
     }
 }
 
+#[derive(Debug, PartialEq, Clone)]
+pub struct Ast {
+    ast: AstNode,
+}
+impl Ast {
+    pub fn emit(&self, context: &Context, builder: &Builder) {
+        self.ast.emit(context, builder);
+    }
+}
+
 pub fn parser(mut tokens: Vec<Token>) -> Ast {
     tokens.reverse();
     let token = tokens.pop();
-    let lhs = match token {
+    let mut lhs = match token {
         Some(token) => match token {
-            Token::Num(_) => AstNum { num: token },
+            Token::Num(_) => AstNode::Num(AstNum { num: token }),
             _ => panic!("Parse Error: Expect a number token."),
         },
         None => panic!("Parse Error: Expect at least one token."),
     };
-    if tokens.len() < 1 {
-        return Ast::Num(lhs);
-    }
-    let token = tokens.pop();
 
-    let op = match token {
-        Some(token) => match token {
-            Token::Op(operator) => match operator.as_ref() {
-                "+" => AstOp {
-                    op: Token::Op(operator),
+    // debug!("FIRST LHS: {:?}", lhs);
+    loop {
+        let token = tokens.pop();
+        let op = match token {
+            Some(token) => match token {
+                Token::Op(operator) => match operator.as_ref() {
+                    "+" => AstOp {
+                        op: Token::Op(operator),
+                    },
+                    _ => panic!("Parse Error: Not implemented oprator."),
                 },
-                _ => panic!("Parse Error: Not implemented oprator."),
+                _ => panic!("Parse Error: Expect operator token, but got otherwise."),
             },
-            _ => panic!("Parse Error: Expect operator token, but got otherwise."),
-        },
-        None => panic!("Parse Error: Expect operator token, but not exist."),
-    };
-    let token = tokens.pop();
-    let rhs = match token {
-        Some(token) => match token {
-            Token::Num(_) => AstNum { num: token },
-            _ => panic!("Parse Error: Expect number token, but got otherwise."),
-        },
-        None => panic!("Parse Error: Expect operator token, but not exist."),
-    };
-
-    Ast::Exp(AstExp { lhs, op, rhs })
+            None => break,
+        };
+        let token = tokens.pop();
+        let rhs = match token {
+            Some(token) => match token {
+                Token::Num(_) => AstNode::Num(AstNum { num: token }),
+                _ => panic!("Parse Error: Expect number token, but got otherwise."),
+            },
+            None => panic!("Parse Error: Expect operator token, but not exist."),
+        };
+        let boxed_lhs = Box::new(lhs);
+        let rhs = Box::new(rhs);
+        lhs = AstNode::Exp(AstBinaryExp {
+            lhs: boxed_lhs,
+            op,
+            rhs,
+        });
+        // debug!("LHS:       {:?}", lhs);
+    }
+    // debug!("LAST LHS:  {:?}", lhs);
+    Ast { ast: lhs }
 }
 
 #[cfg(test)]
@@ -120,9 +146,11 @@ mod tests {
 
     #[test]
     fn test_parser_num() {
-        let expect = Ast::Num(AstNum {
-            num: Token::Num(2434),
-        });
+        let expect = Ast {
+            ast: AstNode::Num(AstNum {
+                num: Token::Num(2434),
+            }),
+        };
         let tokens = vec![Token::Num(2434)];
         let actual = parser(tokens);
         assert_eq!(actual, expect);
@@ -173,17 +201,18 @@ mod tests {
 
     #[test]
     fn test_parser_exp() {
-        let expect = Ast::Exp(AstExp {
-            op: AstOp {
-                op: Token::Op(String::from("+")),
-            },
-            lhs: AstNum {
-                num: Token::Num(10),
-            },
-            rhs: AstNum {
-                num: Token::Num(20),
-            },
-        });
+        let lhs = Box::new(AstNode::Num(AstNum {
+            num: Token::Num(10),
+        }));
+        let op = AstOp {
+            op: Token::Op(String::from("+")),
+        };
+        let rhs = Box::new(AstNode::Num(AstNum {
+            num: Token::Num(20),
+        }));
+        let expect = Ast {
+            ast: AstNode::Exp(AstBinaryExp { lhs, op, rhs }),
+        };
         let tokens = vec![Token::Num(10), Token::Op(String::from("+")), Token::Num(20)];
         let actual = parser(tokens);
         assert_eq!(actual, expect);

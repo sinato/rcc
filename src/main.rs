00481ef68af7eccb3bd83b9df16d34dbd0a6a4e9
Shingo Kato <katomail0510@gmail.com>
2019-04-09 10:12:31 +0900

    make some silly fixes.

--- src/main.rs
+++ src/main.rs
@@ -13,34 +13,32 @@ use parser::ast::AstProgram;
 use parser::emitter::Emitter;
 
 /// EBNF:
-/// program               := function+
-/// function              := type identifier "(" (type val)* ")" "{" statement+ "}"
-/// statement             := [ instruction_statement | compound_statement | if_statement | while_statement ]
-/// compound_statement    := "{" instruction_statement* "}"
-/// if_statement          := "if" "(" condition_statement ")" compound_statement
-/// while_statement       := "while" "(" condition_statement ")" compound_statement
-/// instruction_statement := [ binding_statement | declare_statement | return_statement ]
-/// binding_statement     := identifier "=" val ";"
-/// declare_statement     := [simple_declare_statement | bind_declare_statement]
-/// simple_declare_statement := type identifier ";"
-/// bind_declare_statement   := type identifier "=" val ";"
-/// return_statement      := "return" val ";"
-/// condition_statement   := identifier condition_op val
-/// val                   := [ fin | expression | call ]
-/// fin                   := number | identifier
-/// call                  := (identifier"(") val* ")"
-/// expression            := fin {op fin}
-/// op                    := [ "+" | "*" | "=" ]
-/// condition_op          := "==" | "!="
-/// type                  := "int"
-/// number                := \d+(\.\d)*
-/// identifier            := [a-z]+
+/// program                    := function+
+/// function                   := type identifier "(" (type val)* ")" "{" statement+ "}"
+/// statement                  := [ instruction_statement | compound_statement | if_statement | while_statement ]
+/// compound_statement         := "{" instruction_statement* "}"
+/// if_statement               := "if" "(" condition_statement ")" compound_statement
+/// while_statement            := "while" "(" condition_statement ")" compound_statement
+/// instruction_statement      := [ binding_statement | declare_statement | return_statement ]
+/// binding_statement          := identifier "=" val ";"
+/// declare_statement          := [simple_declare_statement | bind_declare_statement]
+/// simple_declare_statement   := type identifier ";"
+/// bind_declare_statement     := type identifier "=" val ";"
+/// return_statement           := "return" val ";"
+/// condition_statement        := identifier condition_op val
+/// val                        := [ fin | expression | call ]
+/// fin                        := number | identifier
+/// call                       := (identifier"(") val* ")"
+/// expression                 := fin {op fin}
+/// op                         := [ "+" | "*" | "=" ]
+/// condition_op               := "==" | "!="
+/// type                       := "int"
+/// number                     := \d+(\.\d)*
+/// identifier                 := [a-z]+
 fn compiler(code: String) {
     let lexer = Lexer::new();
     let mut tokens = lexer.lex(code);
-    println!("\ntokens: {:?}", tokens);
     let ast = AstProgram::new(&mut tokens);
-    println!("ast: {:?}", ast);
     let mut emitter = Emitter::new();
     emitter.emit(ast);
     emitter.print_to_file("compiled.ll");
